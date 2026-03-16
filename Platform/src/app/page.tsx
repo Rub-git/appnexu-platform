1d4a75ccbf2b70ec3bf0f9f81afb95768aa394fc
Rubisel <rubiesfamily@gmail.com>
2026-03-16 14:29:58 -0700

    fix homepage file extension

---
+++ Platform/src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main style={{padding:40, fontFamily:"sans-serif"}}>
+      <h1>Appnexu</h1>
+      <p>Turn your website into an app in minutes.</p>
+      <a href="/login">Login</a>
+    </main>
+  );
+}
